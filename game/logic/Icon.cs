6bcd334d7917ce62f76a90c7a060f2fc4394e519
galezzza <egor.galeznik.87@gmail.com>
2024-08-07 23:01:15 +0200

    add Globals singleton

--- game/logic/Icon.cs
+++ game/logic/Icon.cs
@@ -15,34 +15,25 @@ public partial class Icon : TextureRect
 		Neutral900,
 		Error
 	}
-
-
+	
 	[Export]
 	private Colors colors = Colors.White;
+
+
 	public override void _Ready()
 	{
 		int index = (int) colors;
 		UpdateIconColor(index);
 	}
-	public override void _Process(double delta)
-	{
-	}
+
 
 	private void UpdateIconColor(int colorIndex)
 	{	
-		Color white = new Color("FFFFFF");
-		Color black = new Color("000000");
-		Color primary = new Color("849BEC");
-		Color secondary = new Color("D9EDFC");
-		Color neutral = new Color("EDEFF8");
-		Color primary900 = new Color("374163");
-		Color neutral900 = new Color("646468");
-		Color error = new Color("BD33A4");
-
-		Color[] colorsList = {white, black, primary, secondary, neutral, primary900, neutral900, error};
+		Color[] colorsList = {Globals.white, Globals.black, Globals.primary, Globals.secondary,
+								 Globals.neutral, Globals.primary900, Globals.neutral900, Globals.error};
 
 		int index = (int) colorIndex;
-		var image = Texture.GetImage();
+
 		SelfModulate = colorsList[index];
 	}
 }
---
+++ game/logic/globals/Globals.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+
+public partial class Globals : Node
+{
+	public static Color white = new Color("FFFFFF");
+	public static Color black = new Color("000000");
+	public static Color primary = new Color("849BEC");
+	public static Color secondary = new Color("D9EDFC");
+	public static Color neutral = new Color("EDEFF8");
+	public static Color primary900 = new Color("374163");
+	public static Color neutral900 = new Color("646468");
+	public static Color error = new Color("BD33A4");
+
+}
